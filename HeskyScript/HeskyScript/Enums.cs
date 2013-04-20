39864be70fa869504904323085a5df11e1b782d0
Rob Rodi <robert.rodi@gmail.com>
2013-04-20 11:46:16 -0700

    add code contracts

--- HeskyScript/HeskyScript/Enums.cs
+++ HeskyScript/HeskyScript/Enums.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -47,8 +48,13 @@ namespace HeskyScript
     {
         private readonly uint _id;
         private readonly uint _count;
+
+        public uint Id { get { return _id; } }
+        public uint Count { get { return _count; } }
+
         public Event(uint id, uint count)
         {
+            Contract.Requires(count > 0);
             _id = id;
             _count = count;
         }
