6fcf65813820104280cd2c266cd0a941c6f5bca2
Rob Rodi <robert.rodi@gmail.com>
2013-04-20 11:50:25 -0700

    Contract test

---
+++ HeskyScript/HeskyScript.Tests/Crap.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeskyScript.Tests
+{
+    [TestClass]
+    public class Crap
+    {
+        [TestMethod]
+        public void Contracts()
+        {
+            try
+            {
+                var e = new Event(0, 0);
+                Assert.Fail("Should Throw");
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
