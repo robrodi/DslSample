520c31b349e1df40106cb8d817002be02013164b
Rob Rodi <robert.rodi@gmail.com>
2013-04-20 11:29:38 -0700

    event

---
+++ HeskyScript/HeskyScript/Enums.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeskyScript
+{
+    public enum Mode
+    {
+        Alpha = 1,
+        Bravo,
+        Charlie,
+        Delta
+    }
+
+    public enum Variant
+    {
+        Echo,
+        Foxtrot,
+        Golf,
+        Hotel
+    }
+
+    public struct Output
+    {
+        private readonly int _cookies, _spaceBucks, _widgets;
+
+        public int Cookies { get { return _cookies; } }
+        public int SpaceBucks { get { return _spaceBucks; } }
+        public int Widgets { get { return _widgets; } }
+
+        public Output(int cookies = 0, int spaceBucks = 0, int widgets = 0)
+        {
+            _cookies = cookies;
+            _spaceBucks = spaceBucks;
+            _widgets = widgets;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} cookies. {1} spacebucks. {2} widgets.", this._cookies, this._spaceBucks, this._widgets);
+        }
+    }
+
+    public struct Event
+    {
+        private readonly uint _id;
+        private readonly uint _count;
+        public Event(uint id, uint count)
+        {
+            _id = id;
+            _count = count;
+        }
+    }
+}
