9ab4b379da01c70807e9a102a6d684767a1e24f6
Rob Rodi <robert.rodi@gmail.com>
2013-04-22 09:17:30 -0700

    move out simple tests

--- DslSample/DslSample.Tests/DslTests.cs
+++ DslSample/DslSample.Tests/DslTests.cs
@@ -210,32 +210,6 @@ namespace DslSample.Tests
         }
     }
 
-    [TestClass]
-    public class SimpleTests
-    {
-        [TestMethod]
-        public void TestExpressionInfoConstructors()
-        {
-            var eventInfo = new TestExpressionInfo(EventCriteria.Id, Condition.Equal, "5");
-            eventInfo.Condition.Should().Be(Condition.Equal);
-            eventInfo.Event.Should().Be(EventCriteria.Id);
-            eventInfo.Input.Should().Be(InputCriteria.None);
-            eventInfo.Source.Should().Be(TestExpressionInfo.ComparisonSource.Event);
-
-            var inputInfo = new TestExpressionInfo(InputCriteria.Mode, Condition.Equal, "5");
-            inputInfo.Condition.Should().Be(Condition.Equal);
-            inputInfo.Event.Should().Be(EventCriteria.None);
-            inputInfo.Input.Should().Be(InputCriteria.Mode);
-            inputInfo.Source.Should().Be(TestExpressionInfo.ComparisonSource.Input);
-        }
-
-        [TestMethod]
-        public void OutputToString()
-        {
-            new Output(1, 2, 3).ToString().Should().Be("1 cookies. 2 spacebucks. 3 widgets.");
-        }
-    }
-
     public abstract class TestBase
     {
         protected const string simplestRule = "when id is 3 add spacebuck";
