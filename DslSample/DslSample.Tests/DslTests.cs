77f6149bbe2f0b0ff432c6208fa0bebac6f2f91e
Rob Rodi <robert.rodi@gmail.com>
2013-04-22 09:18:11 -0700

    more movin

--- DslSample/DslSample.Tests/DslTests.cs
+++ DslSample/DslSample.Tests/DslTests.cs
@@ -209,14 +209,4 @@ namespace DslSample.Tests
             output.SpaceBucks.Should().Be(2);
         }
     }
-
-    public abstract class TestBase
-    {
-        protected const string simplestRule = "when id is 3 add spacebuck";
-        protected static Output Run(IEnumerable<Event> events, string rule = simplestRule, Mode mode = Mode.Charlie, Variant variant = Variant.Foxtrot)
-        {
-            var input = new Input(mode, variant);
-            return new Engine(rule).Run(events, input);
-        }
-    }
 }
---
+++ DslSample/DslSample.Tests/TestBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DslSample.Tests
+{
+    public abstract class TestBase
+    {
+        protected const string simplestRule = "when id is 3 add spacebuck";
+        protected static Output Run(IEnumerable<Event> events, string rule = simplestRule, Mode mode = Mode.Charlie, Variant variant = Variant.Foxtrot)
+        {
+            var input = new Input(mode, variant);
+            return new Engine(rule).Run(events, input);
+        }
+    }
+}
